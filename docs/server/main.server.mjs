e35b44db2a3544c0a42e80910cc8e916451f40ec
msroute42 <msroute42@gmail.com>
2024-07-19 21:19:12 +0300

    upload

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-H52Z6ENY.mjs";import"./chunk-ZC7L3NEU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+// TyprScript 
+export class HomeComponent {
+  // username : string = "Ahmed";
+  // userage : number = 23;
+  // isAdmin : boolean  = false ;
+  // x : undefined = undefined ;
+  // y : null = null;
+  // friends : string[] = ["Ahmed" , "Mohamed" , "yara"]
+  // ages : number[] = [12 ,14]
+  // // typle take by arrange string , number
+  // test : [string , number , boolean] = ["ahmed " , 3 , true]
+  // //union
+  // demo: string | number = "ahmed"
+  // //void
+  // t():void
+  // {
+
+  // }
+  // userName : string = "sara";
+  // imgPath : string = "../../assets/images (1).jpeg";
+  // test(){
+  //   alert(`hello ya ${this.userName}`)
+  // }
+  // isAdmin : boolean = false
+  // userword:string = ''
+}
